2f0cca11a5ec8205dccefe9a9e6d5bdd07586f04
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-03-31 11:05:54 -0500

    Added javascript menu closing function

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const hamburgerDrawer = document.querySelector(".drawer")
+
+hamburgerDrawer.addEventListener("click", (event) => {
+    document.getElementById('hamburger-toggle').checked=false;
+});
