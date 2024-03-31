ef84f9fa9b7e2ce8d7ec88e64a605fe71c1a2929
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-03-31 13:12:53 -0500

    Updated CSS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const hamburgerDrawer = document.querySelector(".drawer")
 
+// When anywhere in the hamburger drawer is clicked, it unchecks the checkbox and closes the hamburger menu.
 hamburgerDrawer.addEventListener("click", (event) => {
     document.getElementById('hamburger-toggle').checked=false;
 });
+
